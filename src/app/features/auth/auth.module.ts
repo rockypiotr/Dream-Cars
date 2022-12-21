379297754af73d28f03e87afb08ee88747c8783c
rockypiotr <rockypiotr72@gmail.com>
2022-12-21 19:12:30 +0100

    Moved auth.service.ts to app\core\services, set new destination of this file and removed unused import

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -12,7 +12,7 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {FormsModule} from "@angular/forms";
-import {AuthService} from "./auth.service";
+import {AuthService} from "../../core/services/auth.service";
 import { RegisterComponent } from './register/register.component';
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 
--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../auth.service";
-import {catchError} from "rxjs";
+import {AuthService} from "../../../core/services/auth.service";
 
 @Component({
   selector: 'app-login',
