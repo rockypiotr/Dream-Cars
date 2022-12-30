4f02c5a06312a445cc3c1bdcd56b79a948bf2347
rockypiotr <rockypiotr72@gmail.com>
2022-12-30 21:31:46 +0100

    Hide Api keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { firebase } from "../../firebase.environment";
 
 export const environment = {
   production: false,
-  firebase: {
-    projectId: 'dreamcars-3608b',
-    appId: '1:530182178392:web:b3db95fd3b46a86c0d6c2d',
-    databaseURL: 'https://dreamcars-3608b-default-rtdb.firebaseio.com',
-    storageBucket: 'dreamcars-3608b.appspot.com',
-    apiKey: 'AIzaSyDF3icIjHHzwm6qd1K6PpsYysAD6BMLXt8',
-    authDomain: 'dreamcars-3608b.firebaseapp.com',
-    messagingSenderId: '530182178392',
-  }
+  ...firebase
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
