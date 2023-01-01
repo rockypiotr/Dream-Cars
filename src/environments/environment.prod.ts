cd211ad468180b77bd50b7dbcd07175fc6e50158
rockypiotr <rockypiotr72@gmail.com>
2023-01-01 23:00:16 +0100

    Hide API keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
+import {firebase} from "../../firebase.environment";
+
 export const environment = {
-  firebase: {
-    projectId: 'dreamcars-3608b',
-    appId: '1:530182178392:web:b3db95fd3b46a86c0d6c2d',
-    databaseURL: 'https://dreamcars-3608b-default-rtdb.firebaseio.com',
-    storageBucket: 'dreamcars-3608b.appspot.com',
-    apiKey: 'AIzaSyDF3icIjHHzwm6qd1K6PpsYysAD6BMLXt8',
-    authDomain: 'dreamcars-3608b.firebaseapp.com',
-    messagingSenderId: '530182178392',
-  },
+  ...firebase,
   production: true
 };
